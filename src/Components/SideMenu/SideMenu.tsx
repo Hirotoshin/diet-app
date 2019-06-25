43041db76b389cbf94f6ca44002461549074ec44
Hirotoshin <hirotoshin0211@gmail.com>
2019-06-26 00:09:56 +0900

    sidemenuボタンの追加

--- src/Components/SideMenu/SideMenu.tsx
+++ src/Components/SideMenu/SideMenu.tsx
@@ -3,8 +3,10 @@ import * as React from "react";
 class SideMenu extends React.Component<{}, {}> {
   public render() {
     return (
-      <div>
-        <p>sidemenu</p>
+      <div className="tabs">
+        <button className="tab_item">食事登録</button>
+        <button className="tab_item">体重登録</button>
+        <button className="tab_item">体重カレンダー</button>
       </div>
     );
   }
